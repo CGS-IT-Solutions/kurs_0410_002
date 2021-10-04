6310fb6a7744a0979ba664316df0755091c165f6
Wuifal <91884796+Kurs2@users.noreply.github.com>
2021-10-04 16:17:32 +0200

    Update test.java

--- src/main/java/com/ats/test.java
+++ src/main/java/com/ats/test.java
@@ -5,10 +5,6 @@ import java.sql.SQLOutput;
 public class test {
     public static void main(String[] args){
 
-
-//OKOK TEST
-
-
         System.out.println("Hello World, this Wolfgang speaking!");
         System.out.println("I can calc funny things: " + MyLib.sum(5,5));
     }
