0f0cdca527f8e361b799d03bf3766f01ee1db291
Kurs2 <cgs.kurs002@gmail.com>
2021-10-04 15:06:41 +0200

    added new fany class for nothing.

--- src/main/java/com/ats/test.java
+++ src/main/java/com/ats/test.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 
 public class test {
     public static void main(String[] args){
-        System.out.println("Hello World, this Kury speaking");
+        System.out.println("Hello World, this Wolfgang speaking!");
     }
 
 }
